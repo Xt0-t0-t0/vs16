db6054bdb5539e6af061da38678e3c4c8ccbfadd
Xt0-t0-t0 <hxdjcefggdgg@gmail.com>
2024-10-19 19:35:25 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Такого нет!")
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+#Запуск страницы с контентом
+@app.route('/form', methods = ["POST"])
+def form():
+    email = request.form.get('email')
+    text = request.form.get('text')
+    print(email, text)
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
